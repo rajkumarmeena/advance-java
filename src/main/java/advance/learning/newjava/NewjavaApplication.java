9b858811bb039e3adb5f3b80ee61b36d59f3c4ab
Raj <rajkumarmeena.iitm@gmail.com>
2019-09-22 00:17:17 +0530

    Intial commit for advanced java project

---
+++ src/main/java/advance/learning/newjava/NewjavaApplication.java
@@ -0,0 +1,13 @@
+package advance.learning.newjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewjavaApplication.class, args);
+	}
+
+}
